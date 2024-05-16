0b17d08076551934983521d47f409b6a8677d210
saeram <seram1427@gmail.com>
2024-05-16 23:33:51 +0900

    updated the functions

--- src/components/Trivia.tsx
+++ src/components/Trivia.tsx
@@ -2,33 +2,85 @@ import { useState } from "react";
 import { quiz } from "./constants";
 import "./Trivia.css";
 
-
 function Trivia() {
-  const [currentQuestion, setCurrentQuestion] = useState(0);
-  const [answerIndex, setAnswerIndex] = useState(0);
+  const [current, setCurrent] = useState(0);
+  const [selectedIndex, setSelectedIndex] = useState(-1);
   const [answer, setAnswer] = useState(false);
+  const [score, setScore] = useState(0);
+  const [submitted, setSubmitted] = useState(false);
+  const [showResult, setShowResult] = useState(false);
 
-  const { question, choices, correctAnswer } = quiz[currentQuestion];
-  const onAnswerClick = (choice:string, index:number) => {
-    setAnswerIndex(index);
+  const { question, choices, correctAnswer } = quiz[current];
+
+  const onAnswerClick = (choice: string, index: number) => {
+    setSelectedIndex(index);
     if (choice === correctAnswer) {
       setAnswer(true);
     } else {
       setAnswer(false);
     }
   };
+  const onPrev = () => {
+    setSelectedIndex(-1);
+    setCurrent(current - 1);
+    setSubmitted(true);
+  };
+
+  const onNext = () => {
+    setSelectedIndex(-1);
+    if (current !== quiz.length - 1) {
+      setCurrent(current + 1);
+    } else {
+      setCurrent(0);
+      setShowResult(true);
+    }
+
+    setSubmitted(false);
+  };
+
+  const onSubmit = () => {
+    setSubmitted(true);
+    {
+      answer ? setScore(score + 1) : setScore(score);
+    }
+  };
 
   return (
     <>
       <div className="quiz-container">
-        <span className="active-question">{currentQuestion + 1}</span>
+        <span className="active-question">{current + 1}</span>
         <span className="total-question">/{quiz.length}</span>
         <h2>{question}</h2>
-        <ul>
+        <ol>
           {choices.map((choice, index) => (
-            <li key={choice} onClick={() => onAnswerClick(choice, index)}>{index + 1}.{choice}</li>
+            <li
+              key={choice}
+              onClick={() => onAnswerClick(choice, index)}
+              className={selectedIndex === index ? "selected-index" : ""}>
+              {choice}
+            </li>
           ))}
-        </ul>
+        </ol>
+        {submitted && !answer ? (
+          <h3>Uh-oh</h3>
+        ) : submitted && answer ? (
+          <h3>You got it!</h3>
+        ) : (
+          <></>
+        )}
+
+        <div className="footer">
+          <button onClick={onPrev} disabled={current === 0}>
+            Previous
+          </button>
+          <button onClick={onSubmit} disabled={submitted}>
+            Submit
+          </button>
+
+          <button onClick={onNext} disabled={!submitted}>
+            {current === quiz.length - 1 ? "Finish" : "Next"}
+          </button>
+        </div>
       </div>
     </>
   );
